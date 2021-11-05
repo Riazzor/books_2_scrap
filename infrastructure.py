1c9dfd692722c88d5b6bb62bca74a003a7415a95
Riazzor <flo.sanika+@gmail.com>
2021-11-06 00:41:13 +0100

    [dict2csv] save to csv
    will save the books in a csv file with category as file_name

---
+++ infrastructure.py
@@ -0,0 +1,34 @@
+import csv
+import os
+
+
+def save_book_to_csv_file(book_info: dict, category: str = 'uncategorized') -> None:
+    """One csv file per category with each book in it."""
+    csv_columns = book_info.keys()
+    csv_file = f"data/{category}.csv"
+
+    # If file doesn't exist, we need to write the header first :
+    if os.path.exists(csv_file) and os.path.getsize(csv_file) != 0:
+        with open(csv_file, 'a') as csv_file:
+            writer = csv.DictWriter(
+                csv_file,
+                fieldnames=csv_columns
+            )
+            writer.writerow(book_info)
+    else:
+        with open(csv_file, 'w') as csv_file:
+            writer = csv.DictWriter(
+                csv_file,
+                fieldnames=csv_columns
+            )
+            writer.writeheader()
+            writer.writerow(book_info)
+
+
+if __name__ == "__main__":
+    from book_page import get_book_info
+    book_info = get_book_info(
+        'http://books.toscrape.com/catalogue/the-requiem-red_995/index.html'
+    )
+    # print(book_info)
+    save_book_to_csv_file(book_info)
