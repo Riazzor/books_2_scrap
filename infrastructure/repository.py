5aa916c5837bac1155bb23061e3ddf67aa39e2c4
Riazzor <flo.sanika+@gmail.com>
2021-11-25 14:57:43 +0100

    [image_download] Code modification
    If file is downloaded, a new field image_path is created and image_url remains the same
    Image name is now the book's UPC.

--- infrastructure/repository.py
+++ infrastructure/repository.py
@@ -1,23 +1,16 @@
 import csv
 import os
-import re
 
 from web_scraping.book_page import get_book_info
 from web_scraping.client import get_book_cover
 
 
-def save_image(image_url: str, image_name: str, category_name: str):
+def save_image(image_url: str, image_name: str, category_name: str) -> str:
     """
     Function save image in Data_image and return path to image
     """
-    # on first book of category, create Data/image/category
-    category_name = category_name.replace(' ', '_')
-    if not os.path.exists(f"./Data/image/{category_name}/"):
-        os.makedirs(f"./Data/image/{category_name}/")
 
     content = get_book_cover(image_url)
-    # replace none alphanumerical character with '_'
-    image_name = re.sub(r'[^\w]+', '_', image_name)
     image_path = f'Data/image/{category_name}/{image_name}.jpg'
     with open(image_path, 'wb') as image_file:
         image_file.write(content)
@@ -31,7 +24,6 @@ def save_book(book_info: dict, category: str = 'uncategorized') -> None:
     csv_columns = book_info.keys()
     csv_file = f"Data/book/{category}.csv"
 
-    # If file doesn't exist, we need to write the header first :
     if not os.path.exists(csv_file):
         with open(csv_file, 'w') as file:
             writer = csv.DictWriter(
--- main.py
+++ main.py
@@ -7,34 +7,40 @@ from web_scraping.category_page import get_category_books
 # Function doesn't support categorie update with new books
 soup = get_web_page(BASEURL)
 category_list = soup.find('ul', class_='nav-list').find('ul').find_all('li')
+
+
 download_image = ''
-while download_image not in ('Yes', 'No'):
-    download_image = 'No'
+while download_image not in ('Oui', 'Non'):
+    download_image = 'Non'
     download_image = input(
-        'Télécharger les images de couvertures ?[yes/No]\n'
+        'Télécharger les images de couvertures ? Si non, le programme sera plus rapide [oui/NON]\n'
     ).capitalize() or download_image
 
 image_path = {
-    'Yes': save_image,
-    'No': lambda image_url, image_name, category_name: False
+    'Oui': save_image,
+    'Non': lambda image_url, image_name, category_name: False
 }
 
-# On first book create Data/book/
-if not os.path.exists("./Data/book/"):
-    os.makedirs("./Data/book/")
+# Before starting create Data/book/ and Data/image/
+if not os.path.exists('./Data'):
+    os.makedirs('./Data/book/')
+    os.makedirs('./Data/image/')
 
 for category in category_list:
     link = category.find('a')
     cat_url = BASEURL + link['href']
-    category_name = link.text.strip()
+    category_name = link.text.strip().replace(' ', '_')
     category_books = get_category_books(cat_url)
+    if download_image == 'Oui':
+        os.makedirs(f'./Data/image/{category_name}/')
     for book in category_books:
-        # If the user's' choice is yes, download image and put image_path in book['image_url']
+        # If the user's' choice is yes, download image and put image_path in book['image_path']
         # else keep image url
         image = image_path.get(download_image)(
             book['image_url'],
-            book['title'],
+            book['universal_product_code'],
             category_name
         )
-        book['image_url'] = image or book['image_url']
+        if image:
+            book['image_path'] = image
         save_book(book, category_name)
